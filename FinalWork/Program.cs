81c2043c82402e1cb337b8c9db249c7a55f1ab05
Alchemist <viktor-avas@yandex.ru>
2022-11-30 23:06:32 +0300

    added Program.cs with the code with the solution of the problem

---
+++ FinalWork/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input a {i+1} value: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+string[] ThreeSymbolsArray(string[] array)
+{
+    string[] resultarray = new string[array.Length];
+    int j = 0;
+        for (int i = 0; i < array.Length; i++)
+            if (array[i].Length <= 3 )
+            {
+                resultarray[j] = array[i];
+                j++;
+            }
+    return resultarray;
+}
+
+void ShowArray(string[] array)
+{
+    Console.Write("Array is: ");
+
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " , ");
+    Console.Write("   <]:-o) \n");
+}
+
+Console.WriteLine("Input the number of rows: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = CreateStringArray(sizeArray);
+ShowArray(myArray);
+string[] finalarray = ThreeSymbolsArray(myArray);
+ShowArray(finalarray);
